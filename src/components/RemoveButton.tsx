d7527ce3c578f092e669a1a8660424bb1b5c25ca
KesseT <64742407+KetCode@users.noreply.github.com>
2024-03-18 16:35:54 -0300

    fix: remove button component

--- src/components/RemoveButton.tsx
+++ src/components/RemoveButton.tsx
@@ -1,14 +1,14 @@
 import MaterialIcons from "@expo/vector-icons/MaterialIcons"
 import { colors } from "@/styles/colors"
-import { TouchableOpacity } from "react-native"
+import { TouchableOpacity, TouchableOpacityProps } from "react-native"
 
-type Props = {
+type Props = TouchableOpacityProps & {
   onRemove: () => void
 }
 
-export function RemoveButton({onRemove}: Props) {
+export function RemoveButton({ onRemove, ...rest }: Props) {
   return (
-    <TouchableOpacity onPress={onRemove}>
+    <TouchableOpacity onPress={onRemove} {...rest}>
       <MaterialIcons name="highlight-remove" size={36} color={colors.red[500]} />
     </TouchableOpacity>
   )
