571fc55b00cc202e94654472108fb2967a96a5d5
KesseT <64742407+KetCode@users.noreply.github.com>
2024-03-18 16:40:35 -0300

    imp: alert style

---
+++ src/utils/alertStyles.ts
@@ -0,0 +1,22 @@
+export const alertStyle = {
+    blue: {
+      body: "bg-blue-100 flex-row p-4 rounded-md absolute bottom-3",
+      title: "text-blue-900 font-medium",
+      content: "text-blue-800 text-sm"
+    },
+    green: {
+      body: "bg-green-100 flex-row p-4 rounded-md m-5 absolute bottom-3",
+      title: "text-green-900 font-medium mb-2",
+      content: "text-green-800 text-sm"
+    },
+    orange: {
+      body: "bg-orange-100 flex-row p-4 rounded-md absolute bottom-3",
+      title: "text-orange-900 font-medium mb-2",
+      content: "text-orange-800 text-sm"
+    },
+    red: {
+      body: "bg-red-100 flex-row p-4 rounded-md absolute bottom-28",
+      title: "text-red-900 font-medium mb-2",
+      content: "text-red-800 text-sm"
+    },
+  }
